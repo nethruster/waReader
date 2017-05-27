ff00eb2764adfc7458ee52ce5fe94d0c0d4e51f4
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-05-27 16:47:52 +0200

    Moved from webpack to gulp (browserify)

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      babel        = require('gulp-babel'),
+      uglify       = require('gulp-uglify'),
+      vueify       = require('vueify'),
+      source       = require("vinyl-source-stream"),
+      buffer       = require("vinyl-buffer"),
+      babelify     = require('babelify'),
+      gulpCopy     = require('gulp-copy'),
+      browserify   = require("browserify"),
+      browserSync = require('browser-sync').create(),
+      autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', () => {
+  return gulp.src('./src/styles/styles.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.stream());;
+});
+
+gulp.task("buildjs", function () {
+    return browserify({
+        "entries": ['./src/index.js'],
+        "debug": true
+    })
+    .transform(babelify.configure({
+        presets: ["es2015"]
+    }))
+    .transform(vueify)
+    .bundle()
+    .pipe(source("waReader.js"))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browserSync.stream());;
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./src/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('js:watch', () => {
+    gulp.watch('./src/helpers/**/*.js', ['buildjs']);
+    gulp.watch('./src/index.js', ['buildjs']);
+});
+
+gulp.task('vue:watch', () => {
+    gulp.watch('./src/views/**/*.vue', ['buildjs']);
+});
+
+gulp.task('serve', ['sass:watch', 'js:watch', 'vue:watch'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("./index.html").on('change', browserSync.reload);
+});
+
+gulp.task('build', ['sass', 'buildjs'], () => {
+    return gulp.src('./src/index.html')
+        .pipe(gulpCopy('./dist/', { prefix: 1 }))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default',  ['serve']);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 
-import "./styles/styles.scss";
-
 import Main from './views/Main.vue'
 
 new Vue(Main).$mount('#app-root')
