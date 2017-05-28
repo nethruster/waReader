004b0829c3497e2f886e342afc2218f0187afd4f
Claudio4 <me@claudio4.com>
2017-05-28 02:36:38 +0100

    Add 'addAnchorLinksToUrls' and 'getEmojiUrl' methods
    Add 'addAnchorLinksToUrls' and 'getEmojiUrl' methods
    Modify 'parseTextFile' regex

--- src/helpers/FileParser.js
+++ src/helpers/FileParser.js
@@ -1,10 +1,22 @@
+import anchorme from "anchorme";
+import twemoji from "twemoji";
+
+function addAnchorLinksToUrls(text) {
+  return anchorme(text);
+}
+function getEmojiUrl(iconId, options) {
+  return `https://twemoji.maxcdn.com/svg/${iconId}.svg`;
+}
+function replaceEmojiWithImg(text) {
+  return twemoji.parse(text,getEmojiUrl);
+}
 function parseTextFile(text) {
   var linesArray = text.split('\n'),
       messages = [],
       userList = [];
   linesArray.forEach((line) => {
-    if (/^(((\d+)(\/)(\d+)(\/)(\d+))(, )((\d+)(:)(\d+))( - )(.*)(:)(\s)(.*))/g.test(line)) {
-      let lineData = /^(((\d+)(\/)(\d+)(\/)(\d+))(, )((\d+)(:)(\d+))( - )(.*)(:)(\s)(.*))/g.exec(line);
+    if (/^(((\d+)(\/)(\d+)(\/)(\d+))(, )((\d+)(:)(\d+))( - )([^:]*)(:)(\s)(.*))/g.test(line)) {
+      let lineData = /^(((\d+)(\/)(\d+)(\/)(\d+))(, )((\d+)(:)(\d+))( - )([^:]*)(:)(\s)(.*))/g.exec(line);
       let obj = {
         date: lineData[2],
         msg: lineData[17],
