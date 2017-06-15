f6a438f962527408b53762addb643009b8ea55b7
Claudio4 <me@claudio4.com>
2017-06-15 22:54:18 +0100

    Add print.js

---
+++ src/helpers/print.js
@@ -0,0 +1,27 @@
+function generateMessagesHtmlString(msgs, selfUser) {
+  var stringDataArray = Array(msgs.length);
+  var lastUser;
+  msgs.forEach(function(e, index) {
+    let bodydata;
+    if(e.user !== '') {
+      bodydata = `<div class="wr-chat-bubble" class="${ msgs[index + 1].user !== e.user ? 'lastByUser' : ''}">
+            ${lastUser !== e.user ? `<span class="wr-chat-message-author">${e.user}</span>` : ''}
+            <div class="wr-chat-message-text">${e.msg}</div>
+            <p class="wr-chat-message-datetime">${ e.datetime }</p>
+        </div>`
+    }else {
+      bodydata = `<div class="wr-chat-sys-message">
+            <p>${e.msg}</p>
+        </div>`;
+    }
+    stringDataArray[index] = `<div class="wr-chat-message flex flex-cross-center ${selfUser === e.user ? 'wr-chat-self' : ''}">${bodydata}</div>`
+   lastUser = e.user;
+  }, this);
+  return stringDataArray.join('');
+}
+const generatePrintViewString = function(msgs, selfUser) {
+
+}
+export default {
+
+}
--- src/views/components/chatMessage.vue
+++ src/views/components/chatMessage.vue
@@ -1,6 +1,6 @@
 <template>
    <div :class="['wr-chat-message', 'flex', 'flex-cross-center', {'wr-chat-self': msg.user == selfUser}]">
-        <div class="wr-chat-bubble" v-if="!(msg.user === '')" :class="{'lastByUser': isLastByUser}">
+        <div class="wr-chat-bubble" v-if="msg.user !== ''" :class="{'lastByUser': isLastByUser}">
             <span class="wr-chat-message-author" v-if="isGroupChat && showAuthor" :style="{color: this.colour}">{{msg.user}}</span>
             <div class="wr-chat-message-text" v-html="msg.msg"></div>
             <p class="wr-chat-message-datetime">{{ msg.datetime }}</p>
