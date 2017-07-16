65021f507062d9fdc712259529e8001bf0fee58f
Guillem Arias Fauste <gariasfa7@alumnes.ub.edu>
2017-07-16 12:55:19 +0200

    Fix render issues on Firefox Change clear date icon by "Clear" string

--- src/views/components/uploadFile.vue
+++ src/views/components/uploadFile.vue
@@ -8,9 +8,7 @@
                 <div class="date-interval-picker-c">
                     <label class="date-interval-picker-label">Start Date 
                         <span class="date-interval-picker-clear-button pointer" v-on:click="clearStart" v-if="intitalDateTime.time !== null">
-                            <svg viewBox="0 0 24 24" class="pointer">
-                                <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                            </svg>
+                            <small class="pointer">&nbsp;&nbsp;CLEAR</small>
                         </span>
                     </label>
                     <date-picker :date="intitalDateTime" :option="pickerOptions" />
@@ -18,9 +16,7 @@
                 <div class="date-interval-picker-c">
                     <label class="date-interval-picker-label">End Date
                         <span class="date-interval-picker-clear-button pointer" v-on:click="clearEnd" v-if="finalDateTime.time !== null">
-                            <svg viewBox="0 0 24 24" class="pointer">
-                                <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                            </svg>
+                            <small class="pointer">&nbsp;&nbsp;CLEAR</small>
                         </span>
                     </label>
                     <date-picker :date="finalDateTime" :option="pickerOptions" />
