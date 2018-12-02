30db2a02d023f73446d2d4e2e13729597f1dd0c4
gariasf <accounts@gariasf.com>
2018-12-02 16:15:37 +0100

    Workspace setup

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
+module.exports = {
+  plugins: [
+    require('cssnano')({
+      autoprefixer: false,
+      discardComments: { removeAll: true }
+    }),
+    postcssPresetEnv({ stage: 0 }),
+    require('autoprefixer')
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production' // Check if we are in production mode
+
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+const APP_DIR = path.resolve(__dirname, 'src')
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  entry: {
+    main: APP_DIR + '/index.js'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /preact|preact-compat|decko|react-ink/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
+  },
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader?modules=true&localIdentName=lsh[hash:base64:7]&minimize: true',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/img/[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader'
+      },
+      {
+        test: /\.(woff2|woff)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/fonts/[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: APP_DIR,
+    compress: true,
+    historyApiFallback: true,
+    stats: 'minimal',
+    hot: true,
+    inline: true,
+    port: 8081,
+    host: '0.0.0.0'
+  },
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
+    },
+    modules: [APP_DIR + '/shared-assets', 'node_modules']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      },
+      hash: true,
+      template: APP_DIR + '/index.html'
+    })
+  ]
+}
