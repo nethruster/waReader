ab915ae76cf5c03524b9d8f94bcaa0e012d7c91a
gariasf <accounts@gariasf.com>
2018-12-02 18:48:02 +0100

    Add base layout and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('waReader')
+import { h, Component, render } from 'preact'
+import 'preact/debug'
+
+import './shared-assets/styles/index.scss'
+
+const mountPoint = document.querySelector('#mount-point')
+
+// App is a Component instance to keep the hot reloading behaviour working
+class App extends Component {
+  render() {
+    return (
+      <div class="app-wrapper">
+        <p>waReader</p>
+      </div>
+    )
+  }
+}
+
+render(<App />, mountPoint, mountPoint.lastElementChild)
