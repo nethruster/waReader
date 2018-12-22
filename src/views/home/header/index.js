43d9b684eb7b5a874f5c2a0a2259521f14ed28e7
gariasf <accounts@gariasf.com>
2018-12-22 14:11:15 +0100

    Hash home header logo

--- src/views/home/header/index.js
+++ src/views/home/header/index.js
@@ -2,11 +2,14 @@ import { h } from 'preact'
 
 import style from './styles.scss'
 
+// Notice me file-loader (づ｡◕‿‿◕｡)づ
+import whiteBanner from '../../../assets/img/banner-white.svg'
+
 export default function Home() {
   return (
     <div class={`flex flex-full-center ${style.header}`}>
       <div class={style.headerLogo}>
-        <img src="./assets/img/banner-white.svg" />
+        <img src={whiteBanner} />
       </div>
     </div>
   )
