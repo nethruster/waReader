3116e93dbc4fa34475a6ccefacdc22991d8d5a7b
gariasf <accounts@gariasf.com>
2019-01-17 22:22:47 +0100

    Tweak home header height
    Change header tag for an actual header tag

--- src/views/home/header/index.jsx
+++ src/views/home/header/index.jsx
@@ -4,10 +4,10 @@ const style = require('./styles.scss');
 
 export default function HomeHeader() {
   return (
-    <div className={`flex flex-full-center ${style.header}`}>
+    <header className={`flex flex-full-center ${style.header}`}>
       <div className={style.headerLogo}>
         <img src="./assets/img/banner-white.svg" />
       </div>
-    </div>
+    </header>
   );
 }
