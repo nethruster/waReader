35d919aef54d89961c10eda5b283d60435ca4b3f
gariasf <accounts@gariasf.com>
2019-01-17 22:22:14 +0100

    Implement IconButton component

---
+++ src/components/icon-button/index.jsx
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+
+import Icon from '../icon';
+
+const style = require('./styles.scss');
+
+export default function Button({
+  type = 'default',
+  size = 24,
+  color,
+  icon,
+  onClickExecute = () => {},
+  customClass,
+  disabled
+}) {
+  return (
+    <button
+      type={type}
+      className={`${style.iconButton} ${customClass ? customClass : ''}`}
+      onClick={onClickExecute}
+      disabled={disabled}
+    >
+      <Icon name={icon} size={size} color={color} />
+    </button>
+  );
+}
