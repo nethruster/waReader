0ccc3ddd199f69c031b00aa00011922eb4822881
gariasf <accounts@gariasf.com>
2019-05-12 21:11:23 +0200

    Update iOs info

--- src/views/home/howto/platforms/iOS/index.js
+++ src/views/home/howto/platforms/iOS/index.js
@@ -7,16 +7,11 @@ export default function IOSHowTo() {
     <ul class={`flex flex-cross-center flex-sa ${style.stepContainer}`}>
       <li class={`flex flex-full-center ${style.step}`}>
         <span class={style.stepNumber}>1</span>
-        <p class={style.stepText}>Open the chat menu</p>
+        <p class={style.stepText}>Go to chat details</p>
       </li>
       <div class={style.divider} />
       <li class={`flex flex-full-center ${style.step}`}>
         <span class={style.stepNumber}>2</span>
-        <p class={style.stepText}>Select "More Options"</p>
-      </li>
-      <div class={style.divider} />
-      <li class={`flex flex-full-center ${style.step}`}>
-        <span class={style.stepNumber}>3</span>
         <p class={style.stepText}>Select "Export chat"</p>
       </li>
     </ul>
